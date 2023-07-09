83648be70352c797eb22629d1fae94df95f81ef6
monisharajan <monisha.rajan@thoughtworks.com>
2023-07-10 00:49:01 +0530

    Create langchain documents and push into vector db

---
+++ ingestion.py
@@ -0,0 +1,40 @@
+import os
+from langchain.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+
+pinecone.init(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+
+def ingest_docs() -> None:
+    loader = ReadTheDocsLoader(path="langchain-docs/api.python.langchain.com/en/latest")
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents)} documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=3000, chunk_overlap=150, separators=["\n\n", "\n", " ", ""]
+    )
+    documents = text_splitter.split_documents(documents=raw_documents)
+    print(f"Splitted into {len(documents)} chunks")
+
+    for doc in documents:
+        old_path = doc.metadata["source"]
+        new_url = old_path.replace("langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to insert {len(documents)} to Pinecone")
+    embeddings = OpenAIEmbeddings()
+    Pinecone.from_documents(
+        documents, embeddings, index_name="langchain-doc-index"
+    )
+
+    print("Persisted vector in vector store")
+
+
+if __name__ == "__main__":
+    print("Hello!!")
+    ingest_docs()
